43d34d5eabbebfd63d9bcad6e1d11bc607794622
synapticweb <synapticwebb@gmail.com>
2020-08-31 03:19:39 +0300

    some di modification

--- app/src/main/java/net/synapticweb/callrecorder/di/RepositoryModule.java
+++ app/src/main/java/net/synapticweb/callrecorder/di/RepositoryModule.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import net.synapticweb.callrecorder.data.Repository;
 import net.synapticweb.callrecorder.data.RepositoryImpl;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -13,6 +15,7 @@ class RepositoryModule {
     private static final String DATABASE_NAME = "callrecorder.db";
 
     @Provides
+    @Singleton
      Repository provideRepository(Context context) {
         return new RepositoryImpl(context, DATABASE_NAME);
     }
